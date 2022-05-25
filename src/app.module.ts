f4c21b9fc3c4655914e4779a913319f852e81bc6
tjehdgur1500 <tjehdgur1500@naver.com>
2022-05-25 11:45:35 +0900

    add client prompt 추가

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { ChatBackEndModule } from './chatBackEnd/chatBackEnd.module';
+import { ChatFrontEndModule } from './chatFrontEnd/chatFrontEnd.module';
+import { DatabaseModule } from './database/database.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {DatabaseService} from "./database/database.service";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import * as Joi from 'joi';
+import {Connection} from "typeorm";
+
+@Module({
+    imports: [
+        ChatBackEndModule, ChatFrontEndModule, DatabaseModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+            validationSchema: Joi.object({
+                DATABASE_HOST: Joi.string().required(),
+                DATABASE_PORT: Joi.number().required(),
+                DATABASE_USER: Joi.string().required(),
+                DATABASE_PASSWORD: Joi.string().required(),
+                DATABASE_NAME: Joi.string().required(),
+            })
+        }),
+        TypeOrmModule.forRoot(
+            new DatabaseService(new ConfigService()).getTypeOrmConfig(),
+        ),
+    ],
+})
+export class AppModule {
+    constructor(private connection: Connection) {
+    }
+}
---
+++ src/database/database.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import {ConfigService} from "@nestjs/config";
+import {TypeOrmModuleOptions} from "@nestjs/typeorm";
+
+@Injectable()
+export class DatabaseService {
+    constructor(private readonly config:ConfigService) {}
+
+    public getTypeOrmConfig(): TypeOrmModuleOptions{
+        return {
+            type: 'mysql',
+            host: this.config.get('DATABASE_HOST'),
+            port: +this.config.get('DATABASE_PORT'),
+            username: this.config.get('DATABASE_USER'),
+            password: this.config.get('DATABASE_PASSWORD'),
+            database: this.config.get('DATABASE_NAME'),
+            entities: [__dirname + '/**/*.entity{.ts,.js}', 'dist/**/*.entity{.ts,.js}'],
+            synchronize: true,
+            autoLoadEntities: true,
+            logging:true
+        }
+
+    }
+}
+
